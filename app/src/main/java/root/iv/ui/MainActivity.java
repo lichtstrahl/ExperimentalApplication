f1d3bce37a670ce6b01fbe0d730ae7d00a114ed1
Игорь Смирнов <cool.rainbow2012@yandex.ru>
2019-02-07 16:14:13 +0300

    Что-то начало получаться

--- app/src/main/java/root/iv/ui/MainActivity.java
+++ app/src/main/java/root/iv/ui/MainActivity.java
@@ -9,22 +9,30 @@ import root.iv.R;
 import root.iv.ui.fragment.FragmentCanvas;
 import root.iv.ui.fragment.FragmentWarningAlert;
 import root.iv.ui.fragment.util.MainPagerAdapter;
+import root.iv.ui.tab.TabHolder;
 
 import android.app.Fragment;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v13.FragmentPagerItem;
 import com.ogaclejapan.smarttablayout.utils.v13.FragmentPagerItemAdapter;
 import com.ogaclejapan.smarttablayout.utils.v13.FragmentPagerItems;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SmartTabLayout.TabProvider {
+    @BindView(R.id.tabLayout)
+    SmartTabLayout viewPagerTab;
     @BindView(R.id.viewPager)
     ViewPager viewPager;
-    private FragmentPagerItemAdapter adapter;
-    @BindView(R.id.tabLayout)
-    SmartTabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,16 +40,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        adapter = new FragmentPagerItemAdapter(
-                getFragmentManager(),
-                FragmentPagerItems.with(this)
-                        .add("Canvas", FragmentCanvas.class)
-                        .add("Warning", FragmentWarningAlert.class)
-                        .create()
-        );
+        viewPagerTab.setCustomTabView(this);
+
+        FragmentPagerItems pages = new FragmentPagerItems(this);
+        pages.add(FragmentPagerItem.of("Canvas", FragmentCanvas.class));
+        pages.add(FragmentPagerItem.of("Alert", FragmentWarningAlert.class));
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(getFragmentManager(), pages);
 
         viewPager.setAdapter(adapter);
-        tabLayout.setViewPager(viewPager);
+        viewPagerTab.setViewPager(viewPager);
     }
 
     @Override
@@ -53,5 +61,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return false;
+    }
+
+    @Override
+    public View createTabView(ViewGroup container, int position, PagerAdapter adapter) {
+        LayoutInflater inflater = LayoutInflater.from(container.getContext());
+        Resources res = container.getContext().getResources();
+        View tab = inflater.inflate(R.layout.tab_main, container, false);
+        return tab;
     }
 }
