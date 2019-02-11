40fa10533c4c536f0d4e304f1dc92123f1ad585a
Игорь Смирнов <cool.rainbow2012@yandex.ru>
2019-02-11 19:46:48 +0300

    Добавил бургер-меню

--- app/src/main/java/root/iv/ui/activity/NavigationActivity.java
+++ app/src/main/java/root/iv/ui/activity/NavigationActivity.java
@@ -1,29 +1,37 @@
 package root.iv.ui.activity;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import root.iv.R;
-
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import root.iv.R;
+
 public class NavigationActivity extends AppCompatActivity {
     @BindView(R.id.navigationView)
     NavigationView navigationView;
+    @BindView(R.id.drawerLayout)
+    DrawerLayout drawerLayout;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
         ButterKnife.bind(this);
-
+        setSupportActionBar(toolbar);
         navigationView.setNavigationItemSelectedListener(this::onNavigationItemSelected);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
     }
 
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -35,6 +43,20 @@ public class NavigationActivity extends AppCompatActivity {
                 Toast.makeText(this, "Search", Toast.LENGTH_LONG).show();
                 break;
         }
+
+        // Закрытие меню
+//        drawerLayout.closeDrawer(GravityCompat.START, true);
         return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START, true);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
