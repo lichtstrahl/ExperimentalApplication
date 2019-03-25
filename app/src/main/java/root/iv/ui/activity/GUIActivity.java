237b5837e613ff398b7a0ecb4d3fa61dc0dde5c8
Игорь Смирнов <cool.rainbow2012@yandex.ru>
2019-03-25 11:09:22 +0300

    add Glide

--- app/src/main/java/root/iv/ui/activity/GUIActivity.java
+++ app/src/main/java/root/iv/ui/activity/GUIActivity.java
@@ -1,17 +1,45 @@
 package root.iv.ui.activity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import root.iv.R;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class GUIActivity extends AppCompatActivity {
+    private static final int PICK_IMAGE = 1;
+    @BindView(R.id.preview)
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gui);
         ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.buttonLoad)
+    public void clickLoad() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode) {
+            case PICK_IMAGE:
+                Uri imageUri = data.getData();
+
+                break;
+        }
     }
 }
