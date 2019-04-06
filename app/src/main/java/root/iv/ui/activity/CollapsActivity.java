25483742c84a77519309e73d67c0dff83ea3846e
Игорь Смирнов <cool.rainbow2012@yandex.ru>
2019-04-06 13:26:49 +0300

    Оформил OptionMenu

--- app/src/main/java/root/iv/ui/activity/CollapsActivity.java
+++ app/src/main/java/root/iv/ui/activity/CollapsActivity.java
@@ -1,7 +1,10 @@
 package root.iv.ui.activity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.appbar.AppBarLayout;
 
@@ -27,10 +30,37 @@ public class CollapsActivity extends AppCompatActivity {
 //
 //
         setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
         appBarLayout.addOnOffsetChangedListener((appBar, offset) -> {
             int totalRange = appBar.getTotalScrollRange();
             float a = Math.abs(offset/(float)totalRange);
             shadowView.setAlpha(a*a*a*a);
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottom_navigation_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuNavigationCamera:
+                Toast.makeText(this, "Камера", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.menuNavigationSearch:
+                Toast.makeText(this, "Поиск", Toast.LENGTH_SHORT).show();
+                break;
+
+            case android.R.id.home:
+                Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+
+
 }
